ccd1f9526c570426e0fe27a66741efe5abdb92e0
Lydia003 <azwalidiya03@gmail.com>
2026-07-07 17:53:50 +0800

    delete

--- analytics-service/src/main/java/com/smartcampus/analytics/CourseStatsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smartcampus.analytics;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseStatsRepository extends JpaRepository<CourseStats, String> {}
